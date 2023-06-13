ec8e3086746dd0203b940e4c02c764038da46271
m124578n <m23568n@gmail.com>
2023-06-13 17:14:28 +0800

    flask init with SQLAlchemy and Migrate

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from .extensions import db, migrate
+import os
+
+
+def create_app():
+
+    #  取得目前文件資料夾路徑
+    pjdir = os.path.abspath(os.path.dirname(__file__))
+
+    app = Flask(__name__)
+    #  新版本的部份預設為none，會有異常，再設置True即可。
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #  設置sqlite檔案路徑
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(pjdir, 'data.sqlite')
+
+    db.init_app(app)
+    from . import model
+    migrate.init_app(app, db)
+
+    return app
---
+++ app/model.py
@@ -0,0 +1,16 @@
+from .extensions import db
+
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+    
